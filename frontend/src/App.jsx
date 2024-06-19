24577e3c69f1cdd5c97ec34ecfe2a599883543de
sarthak <bindalsarthak03@gmail.com>
2024-06-19 12:12:52 +0530

    [REFACTOR] routing done for retail and business as per need ✔️

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
-import RegistrationForm from './components/RegistrationForm';
+import SelectionPage from './pages/SelectionPage';
+import RegisterBusiness from './pages/RegisterBusiness';
+import RegisterRetail from './pages/RegisterRetail';
 
 function App() {
   return (
     <>
-    <Header/>
-      <RegistrationForm/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/selection-type' element={<SelectionPage/>}/>
+          <Route path='/register-business' element={<RegisterBusiness/>}/>
+          <Route path='/register-retail' element={<RegisterRetail/>}/>
+          </Routes>
+      </BrowserRouter>
+      
     </>
   )
 }
