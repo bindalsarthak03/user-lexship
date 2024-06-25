0363a7a5b44fd69208a4d5aa8c186053c6a22ca3
sarthak <bindalsarthak03@gmail.com>
2024-06-25 17:21:31 +0530

    [CHORE] minor change in css of spinner 🔨

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@
 import { ClipLoader } from 'react-spinners';
 
 const Spinner = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
     <ClipLoader color={'teal'} loading={true} size={150} />
   </div>
 );
