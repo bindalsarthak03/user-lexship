99d38791c3502bda705b8cf2f48eb5b3283da1ae
Sarthak Bindal <bindalsarthak03@gmail.com>
2024-07-16 17:10:10 +0530

    [REFACTOR] hedear responsiveness added and refactored structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,125 @@
-
-import { IconButton, Image, useColorMode, Icon } from '@chakra-ui/react';
-import { FiSun, FiMoon } from 'react-icons/fi';
+import { useState } from 'react';
+import {
+  IconButton,
+  Image,
+  useColorMode,
+  Icon,
+  Flex,
+  Link,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  VStack,
+  DrawerFooter,
+} from '@chakra-ui/react';
+import { FiSun, FiMoon, FiMenu, FiInstagram } from 'react-icons/fi';
+import { RiFacebookCircleFill, RiTwitterXFill } from 'react-icons/ri'
+import { SiLinkedin } from 'react-icons/si'
 import logo from '../assets/lexship.png';
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useState();
 
   return (
-    <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: '100%' }}>
-      <Image src={logo} alt='Lexship' h={[8, 41]} />
-      <IconButton
-        size={['sm', 'md']}
-        aria-label={`Toggle ${colorMode === 'light' ? 'Dark' : 'Light'} Mode`}
-        icon={colorMode === 'light' ? <Icon as={FiMoon} /> : <Icon as={FiSun} />}
-        onClick={toggleColorMode}
-      />
-    </header>
+    <Flex
+      flexDir={'row'}
+      justifyContent={'space-between'}
+      alignItems={'center'}
+    >
+      <span>
+        <Link href="/">
+          <Image src={logo} alt="Lexship" h={[10, 50]} />
+        </Link>
+      </span>
+      <Flex
+        display={{ base: 'none', md: 'flex' }}
+        flexDir={'row'}
+        justifyContent={'flex-start'}
+        alignItems={'center'}
+        gap={10}
+      >
+        <Link _hover={{ textDecoration: 'underline', color: 'teal.400' }} href="/contact-us">
+          Contact Us
+        </Link>
+        <Link _hover={{ textDecoration: 'underline', color: 'teal.400' }} href="/about-us">
+          About Us
+        </Link>
+        <Link _hover={{ textDecoration: 'underline', color: 'teal.400' }} href="/selection-type">
+          SignUp/SignIn
+        </Link>
+        <IconButton
+          size={['sm', 'md']}
+          aria-label={`Toggle ${colorMode === 'light' ? 'Dark' : 'Light'} Mode`}
+          icon={colorMode === 'light' ? <Icon as={FiMoon} /> : <Icon as={FiSun} />}
+          onClick={toggleColorMode}
+        />
+      </Flex>
+      
+      <Flex gap={2} display={{base:'flex',md:"none"}} flexDir={'row'}>
+        <IconButton
+        display={{base:'flex',md:"none"}}
+          size={['md', 'md']}
+          aria-label={`Toggle ${colorMode === 'light' ? 'Dark' : 'Light'} Mode`}
+          icon={colorMode === 'light' ? <Icon as={FiMoon} /> : <Icon as={FiSun} />}
+          onClick={toggleColorMode}
+        />
+        <IconButton
+          size={['md', 'md']}
+          ref={btnRef}
+          display={{ base: 'flex', md: 'none' }}
+          aria-label="Open menu"
+          icon={<FiMenu />}
+          onClick={onOpen}
+        />
+      </Flex>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+
+      >
+        <DrawerOverlay>
+          <DrawerContent >
+            <DrawerCloseButton />
+            <DrawerHeader borderBottomWidth='1px'>Menu</DrawerHeader>
+            <DrawerBody >
+              <VStack align="flex-start" gap={10} mt={5}>
+                <Link _hover={{ textDecoration: 'underline', color: 'teal.400' }} href="/contact-us">
+                  Contact Us
+                </Link>
+                <Link _hover={{ textDecoration: 'underline', color: 'teal.400' }} href="/about-us">
+                  About Us
+                </Link>
+                <Link _hover={{ textDecoration: 'underline', color: 'teal.400' }} href="/selection-type">
+                  SignUp/SignIn
+                </Link>
+              </VStack>
+            </DrawerBody>
+            <DrawerFooter gap={5} placeContent={'flex-start'} mb={2}>
+              <Link  href='https://www.linkedin.com/company/logilink-india/' >
+                <SiLinkedin fontSize={'20px'} />
+              </Link>
+              <Link href='https://x.com/lexvyu'>
+                <RiTwitterXFill fontSize={'20px'} />
+              </Link>
+              <Link href='https://www.instagram.com/lexshipdotcom/'>
+                <FiInstagram fontSize={'20px'} />
+              </Link>
+              <Link href='https://www.facebook.com/lexshipdotcom/'>
+                <RiFacebookCircleFill fontSize={'20px'} />
+              </Link>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </Flex>
   );
 };
 
