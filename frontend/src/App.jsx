819948cf999b8a03c3c63fb1791ae257af1aeb7d
Sarthak Bindal <bindalsarthak03@gmail.com>
2024-07-16 17:09:31 +0530

    [ADDED] default route added for homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import { Suspense, lazy } from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import Spinner from './components/Spinner';
 import KycBusiness from './pages/KycBusiness';
+import HomePage from './pages/HomePage';
+import ContactUs from './pages/ContactUs';
+import AboutUs from './pages/AboutUs';
 const SelectionPage = lazy(() => import('./pages/SelectionPage'));
 const RegisterBusiness = lazy(() => import('./pages/RegisterBusiness'));
 const RegisterRetail = lazy(() => import('./pages/RegisterRetail'));
@@ -11,18 +14,19 @@ const RegisterRetail = lazy(() => import('./pages/RegisterRetail'));
 function App() {
   return (
     <>
-      <Header />
+      
+      <Header /> 
       <BrowserRouter>
         <Suspense fallback={<Spinner />}>
           <Routes>
-            <Route
-              path="*"
-              element={<Navigate to="/selection-type" replace={true} />}
-            />
+            <Route path='/' element={<HomePage/>}/>
             <Route path='/selection-type' element={<SelectionPage />} />
             <Route path='/register-business' element={<RegisterBusiness />} />
             <Route path='/register-retail' element={<RegisterRetail />} />
             <Route path='/kyc-verification' element={<KycBusiness/>}/>
+            <Route path='/contact-us' element={<ContactUs/>}/>
+            <Route path='/about-us' element={<AboutUs/>}/>
+
           </Routes>
         </Suspense>
       </BrowserRouter>
